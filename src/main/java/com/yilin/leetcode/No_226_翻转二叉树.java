d32c3afba4db5d75cbbb54b06ece9dca2efaf255
yilin <yilin@chen.cn>
2022-09-16 16:26:05 +0800

    第226题 翻转二叉树（完成）

--- src/main/java/com/yilin/leetcode/No_226_翻转二叉树.java
+++ src/main/java/com/yilin/leetcode/No_226_翻转二叉树.java
@@ -1,24 +1,9 @@
 package com.yilin.leetcode;
 
 import com.yilin.utils.TreeNode;
-/**
- * 第226题翻转二叉树
- */
 
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
+ * 第226题翻转二叉树
  */
 public class No_226_翻转二叉树 {
     public static void main(String[] args) {
