530afa175c5f4926b1a9509fb984af20aaade22a
yilin <yilin@chen.cn>
2022-09-19 13:24:26 +0800

    第283题 移动零（完成）

---
+++ src/main/java/com/yilin/leetcode/No_283_移动零.java
@@ -0,0 +1,27 @@
+package com.yilin.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class No_283_移动零 {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            //nums[i]为0，i++；nums[i]不为0，i和j交换，i++，j++；
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
